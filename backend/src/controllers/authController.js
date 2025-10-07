68a0eb1789cd4c3b0209658d550d2ec05a2eabb9
Yash Vimal <yashsarthak79@gmail.com>
2025-10-07 22:41:41 +0530

    update profile endpoint added

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -3,6 +3,7 @@ import User from "../models/User.js"
 import bcrypt from "bcryptjs";
 import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 import { ENV } from "../lib/env.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body
@@ -74,7 +75,7 @@ export const signup = async (req, res) => {
 export const login = async (req, res) => {
     const { email, password } = req.body;
 
-    if (!email || password) {
+    if (!email || !password) {
         return res.status(400).json({ message: "Email and password are required" });
     }
 
@@ -103,4 +104,22 @@ export const login = async (req, res) => {
 export const logout = (_, res) => {
     res.cookie("jwt", "", { maxAge: 0 })
     res.status(200).json({ message: "Logged out successfully!" });
+};
+
+export const updateProfile = async (req, res) => {
+    try {
+        const { profilPic } = req.body;
+        if (!profilPic) return res.status(400).json({ message: "Profile Pic is required" });
+
+        const userId = req.user._id;
+
+        const uploadResponse = await cloudinary.uploader.upload(profilPic);
+
+        const updatedUser = await User.findByIdAndUpdate(userId, { profilPic: uploadResponse.secure_url }, { new: true });
+
+        res.status(200).json(updatedUser)
+    } catch (error) {
+        console.log("Error in Update Profile:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
 };
