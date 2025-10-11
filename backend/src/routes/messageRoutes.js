40a273f0554093720ad8b4529e06201f475639b4
Yash Vimal <yashsarthak79@gmail.com>
2025-10-11 23:10:08 +0530

    message routes added

--- backend/src/routes/messageRoutes.js
+++ backend/src/routes/messageRoutes.js
@@ -1,9 +1,18 @@
 import express from "express";
+import { getAllContacts, getChatPartners, getMessageByUserId, sendMessage } from "../controllers/messageController.js";
+import { protectRoute } from "../middleware/authMiddleware.js";
+import { arcjetProtection } from "../middleware/arcjetMiddleware.js";
 
 const router = express.Router();
 
-router.get("/send", (req, res) => {
-    res.send("send messages")
-});
+
+// the middlewares execute in order - so requests get rate limited first, then authenticated.
+// this is actually more efficient since unauthenticated requests get blocked by rate limiting before hitting the auth middleware.
+router.use(arcjetProtection, protectRoute);
+
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessageByUserId);
+router.post("/send/:id", sendMessage);
 
 export default router;
