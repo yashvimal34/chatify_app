a3f19d40d6cd9ae85f3cd8b3128760324ff83486
Yash Vimal <yashsarthak79@gmail.com>
2025-10-11 23:21:02 +0530

    msgController optimise

--- backend/src/controllers/messageController.js
+++ backend/src/controllers/messageController.js
@@ -43,6 +43,19 @@ export const sendMessage = async (req, res) => {
         const { id: receiverId } = req.params;
         const senderId = req.user._id;
 
+        if (!text && !image) {
+            return res.status(400).json({ message: "Text or image is required." });
+        }
+
+        if (senderId.equals(receiverId)) {
+            return res.status(400).json({ message: "cannot send messages to yourself." });
+        }
+
+        const receiverExists = await User.exists({ _id: receiverId });
+        if (!receiverExists) {
+            return res.status(404).json({ message: "Receiver not found." });
+        }
+
         let imageUrl;
         if (image) {
             // upload base64 image to cloudinary
