9010fbbaabecbe77d1b2676028684a631ec3f261
Yash Vimal <yashsarthak79@gmail.com>
2025-10-16 12:34:28 +0530

    signup implemented on the frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/authRoutes.js"
 import messageRoutes from "./routes/messageRoutes.js"
 import { connectDB } from './lib/db.js';
 import { ENV } from './lib/env.js';
+import cors from "cors";
 
 
 const app = express();
@@ -13,6 +14,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000
 
 app.use(express.json()); // req.body
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }))
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
-import { Route, Routes } from "react-router"
+import { Navigate, Route, Routes } from "react-router"
 import ChatPage from "./pages/Chatpage"
 import LoginPage from "./pages/LoginPage"
 import SignUpPage from "./pages/SignUpPage"
 import { useAuthStore } from "./store/useStoreAuth"
+import { useEffect } from "react"
+import PageLoader from "./components/PageLoader"
+import { Toaster } from "react-hot-toast";
 
 function App() {
 
-  const { authUser, login, isLoggedIn } = useAuthStore()
-  console.log("auth user:", authUser)
-  console.log("isLoggedIn:", isLoggedIn)
+  const { checkAuth, isCheckingAuth, authUser } = useAuthStore()
+
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth])
+
+  if (isCheckingAuth) return <PageLoader />;
 
   return (
     <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
@@ -18,13 +25,12 @@ function App() {
       <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
       <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
 
-      <button onClick={login} className="z-10">Login</button>
-
       <Routes>
-        <Route path="/" element={<ChatPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/" element={authUser ? <ChatPage /> : <Navigate to={"/login"} />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to={"/"} />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />} />
       </Routes>
+      <Toaster />
     </div>
   )
 }
